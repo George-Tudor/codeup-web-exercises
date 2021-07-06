e44490e9e0687d152d5d3dc41cea3fbe5c43cec8
George Tudor <Alexander.George.Tudor@gmail.com>
2021-07-06 16:06:24 -0500

    Completed Javascript with HTML Exercises

--- js/external.js
+++ js/external.js
@@ -2,8 +2,8 @@
 
 alert("Welcome to my Website!");
 
-var color = prompt("What's your favorite color?");
-alert("Great, " + color + " is my favorite color too!");
+var favColor = prompt("What's your favorite color?");
+alert("Great, " + favColor + " is my favorite color too!");
 
 
 var rentalPerDayDollars = prompt("What is the daily rental rate in dollars?");
@@ -27,11 +27,30 @@ var googleHours = prompt("How many hours did you work at Google this week?");
 var amazonHours = prompt("How many hours did you work at Amazon this week?");
 var facebookHours = prompt("How many hours did you work at Facebook this week?");
 var totalPayment;
-totalPayment = googleHours * googleHourlyRateDollars;
-totalPayment += amazonHours * amazonHourlyRateDollars;
-totalPayment += facebookHours * facebookHourlyRateDollars;
+totalPayment = googleHours * googleHourlyRateDollars
+ + amazonHours * amazonHourlyRateDollars
+ + facebookHours * facebookHourlyRateDollars;
 alert("You made $" + totalPayment + " dollars this week!");
 
-var classIsNotFull = confirm("Is class full?"); // boolean
-var classSchedulesCheck = confirm("Are there any conflicts?"); // boolean
+var classIsNotFull = confirm("Does the class have room?"); // boolean
+var classSchedulesCheck = confirm("Is the schedule free of conflicts?"); // boolean
 var studentEnrolled = classIsNotFull && classSchedulesCheck;
+
+if(studentEnrolled) { alert("The student can enroll!")
+    } else {
+    alert("Class is full, or there's a scheduling conflict.");
+}
+
+
+var numberOfItemsForDiscount = prompt("How many items are needed for discount?");
+var numberOfItems = prompt("How many items are there?"); // number
+var offerIsNotExpired = confirm("Click ok if the offer is still valid, or no if it's expired?"); // boolean
+var isPremiumMember = confirm("Click ok for Premium member, and cancel for regular member."); // boolean
+var productDiscountApplied = isPremiumMember || (numberOfItems >= numberOfItemsForDiscount && offerIsNotExpired);
+if(productDiscountApplied) {
+    alert("The discount was applied!");
+} else {
+    alert("Cannot apply discount.")
+}
+
+
