0bad3357ec033ebfbf810cc1e4fbdcd07993c53c
George Tudor <Alexander.George.Tudor@gmail.com>
2021-07-15 14:19:45 -0500

    modified while.js, and completed exercise 1, 2, and 3 of for_loops.js

---
+++ js/for_loops.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// function showMultiplicationTable(number) {
+//     for(var i = 1; i <= 10; i++) {
+//         console.log(number + " x " + i + " = " + number * i);
+//     }
+// }
+// showMultiplicationTable(7);
+
+// function parity() {
+//     for(var i = 1; i <= 10; i++) {
+//         var randomNum = Math.floor(Math.random() * 180) + 20;
+//         if(randomNum % 2 === 0) {
+//             console.log("Pass " + i + " result: " +randomNum + " is even");
+//         } else {
+//             console.log("Pass " + i + " result: " +randomNum + " is odd");
+//         }
+//     }
+// }
+// parity();
+
+for(var i = 1; i < 10; i++) {
+    console.log(i++)
+}
--- js/while.js
+++ js/while.js
@@ -7,20 +7,21 @@
 //     console.log(num);
 // }
 
-var numberOfCones = Math.floor(Math.random() * 50) + 50;
+// var numberOfCones = Math.floor(Math.random() * 50) + 50;
+//
+//
+// do {
+//     var conesPerClient = Math.floor(Math.random() * 5) + 1;
+//
+//     if(numberOfCones < conesPerClient) {
+//         console.log("Cannot sell you " + conesPerClient + " cones, I only have " + numberOfCones + " left.");
+//     } else {
+//         console.log('She has ' + numberOfCones + ' ice cream cones.');
+//         console.log('She just sold ' + conesPerClient + ' cones.');
+//         numberOfCones -= conesPerClient;
+//         console.log('There are ' + numberOfCones + ' remaining.\n\n');
+//     }
+//
+// } while(numberOfCones > 0);
+// console.log("Yay! I sold them all!");
 
-
-do {
-    var conesPerClient = Math.floor(Math.random() * 5) + 1;
-
-    if(numberOfCones < conesPerClient) {
-        console.log("Cannot sell you " + conesPerClient + " cones, I only have " + numberOfCones + " left.");
-    } else {
-        console.log('She has ' + numberOfCones + ' ice cream cones.');
-        console.log('She just sold ' + conesPerClient + ' cones.');
-        numberOfCones -= conesPerClient;
-        console.log('There are ' + numberOfCones + ' remaining.\n\n');
-    }
-
-} while(numberOfCones > 0);
-console.log("Yay! I sold them all!");
