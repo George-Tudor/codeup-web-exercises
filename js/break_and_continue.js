dd645728c6848b3ef94d8188a48cf83729f105f5
George Tudor <Alexander.George.Tudor@gmail.com>
2021-07-18 17:36:06 -0500

    finished the break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,22 @@
 (function toBreakOrContinue() {
-    var isOdd = prompt("Please enter an odd number between 1 and 50: ");
-    if(typeof parseFloat(isOdd) === "number" || isOdd % 2 === 0 || isOdd < 50 || isOdd <= 0) {
-        alert("You must enter an ODD NUMBER between 1 and 50.  Please reload and try again!")
-    }
+    var isItOdd = prompt("Please enter an odd number between 1 and 50: ");
+    isItOdd = Number(isItOdd);
+    do {
+        if(isItOdd <= 50 && isItOdd > 0 && isItOdd % 2 !== 0 && isNaN(parseFloat(isItOdd)) === false) {
+            break;
+        } else {
+            isItOdd = prompt('Invalid input.  Please enter an ODD NUMBER between 1 and 50: ')
+        }
+    } while (isItOdd > 50 || isItOdd <= 0 || isItOdd % 2 === 0 || isNaN(parseFloat(isItOdd)) === true);
 
+    for (var i = 1; i < 50; i += 2) {
+
+        if(i === isItOdd) {
+            console.log('Yikes! Skipping number: ' + i )
+            continue;
+        }
+
+        console.log('Here is an odd number: ' + i )
+    }
 })();
+
